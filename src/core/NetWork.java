4e7b125e369e56c53535302f503545082fe40837
Carl <carle293@hotmail.com>
2012-07-08 23:26:06 +0200

    New class that handles the request to the api
    This class will handle specific requests and return a java class that
    holds the information

---
+++ src/core/NetWork.java
@@ -0,0 +1,124 @@
+package core;
+
+import gui.GistViewer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import pojo.ArrayWrapper;
+import pojo.Gist;
+import pojo.GistLister;
+
+public class NetWork {
+	
+	private ObjectMapper mapper;
+	
+	public NetWork(){
+		mapper = new ObjectMapper();
+	}
+	public Gist getGist(String id) {
+		return getGist(id,"EMPTY");
+	}
+	public Gist getGist(String id,String access_token){
+		String urlcreater = "https://api.github.com/gists/"+id;
+		
+		if(access_token != "EMPTY"){
+			urlcreater = urlcreater + "?access_token=" + access_token;
+		}
+		//Now call the api
+		String response = apiCall(urlcreater);
+		Gist gist = this.jsonToPojo(response, Gist.class);
+		return gist;
+	}
+	
+	/**
+	 * Fetch the the list of all gist's from the user giving in name.
+	 * (Only public gist's) 
+	 * 
+	 * @param name The name of the user
+	 * @param access_token The access token the get private gist's as well
+	 * @return List of the gist's for the user
+	 */
+	public GistLister[] getProfile(String name){
+		return getProfile(name,"EMPTY");
+	}
+	/**
+	 * Fetch the the list of all gist's from the user giving in name.
+	 * (Both public and private gist's) 
+	 * 
+	 * @param name The name of the user
+	 * @param access_token The access token the get private gist's as well
+	 * @return List of the gist's for the user
+	 */
+	public GistLister[] getProfile(String name,String access_token){
+		String urlcreater = "https://api.github.com/users/"+name+"/gists";
+		
+		if(access_token != "EMPTY"){
+			urlcreater = urlcreater + "?access_token=" + access_token;
+		}
+		//Now call the api
+		String response = apiCall(urlcreater);
+		//Convert to the json to java pojo
+		GistLister[] gists = this.jsonToPojo(response, GistLister[].class);
+		
+		return gists;
+		
+	}
+	
+	private <T> T jsonToPojo(String input,Class<T> inputType){
+		try {
+			return inputType.cast(mapper.readValue(input, inputType));
+		} catch (JsonParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	/**
+	 * Makes the url request to the url that is given as input and return the response+
+	 * The return from gist api calls in only one line the method will only return the
+	 * first line from the response
+	 * 
+	 * @param inputURL The address to make the request
+	 * @return The answer or if something went wrong 
+	 */
+	private String apiCall(String inputURL){
+		String response = "EMPTY";
+		try {
+			URL url = new URL(inputURL);
+			URLConnection con  = url.openConnection();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			response = in.readLine();		
+		} catch (MalformedURLException e) {
+			response = "{\"error\": \"Something went wrong with malformedurl\"}";
+			e.printStackTrace();
+		} catch (IOException e) {
+			response = "{\"error\": \"IOExceptions happend\"}";
+			e.printStackTrace();
+		}
+		return response; 
+	}
+	
+	public static void main(String[] args){
+		NetWork nw = new NetWork();
+		
+		//new GistViewer(nw.getGist("2841832"));
+		GistLister[] temp = nw.getProfile("Wneh");
+		System.out.println(temp[0].getDescription());
+	}
+}
