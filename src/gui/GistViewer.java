39f9bef89a6151524110a296370306adac536554
Carl Eriksson <carle293@hotmail.com>
2012-08-31 17:51:24 +0200

    Add: Lists all the files in one gist

--- src/gui/GistViewer.java
+++ src/gui/GistViewer.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 import pojo.Gist;
@@ -16,8 +19,14 @@ public class GistViewer extends JFrame {
 	
 
 	private static final long serialVersionUID = -7319639111951226534L;
+	private static final int JLIST_CELL_WIDTH = 200;
+	
 	private Gist gist;
 	private ArrayList<JTextArea> jtas;
+	
+	private JList fileList;
+	private DefaultListModel fileListModel;
+	private JScrollPane fileListScroll;
 	
 	public GistViewer(Gist gistIn){		
 		this.gist = gistIn;
@@ -25,10 +34,19 @@ public class GistViewer extends JFrame {
 		Container c = getContentPane();
 		c.setLayout(new BorderLayout());
 		
+		//JList 
+		fileListModel = new DefaultListModel();
+	    fileList = new JList(fileListModel);
+	    	fileList.setFixedCellWidth(JLIST_CELL_WIDTH);
+	    fileListScroll = new JScrollPane(fileList);
+	    fillJList();
+	        
+		//JTextArea
 		jtas = new ArrayList<JTextArea>();
 		createAllJTextArea();
 		
-		c.add(jtas.get(0),BorderLayout.CENTER);		
+		c.add(fileListScroll,BorderLayout.WEST);
+		c.add(jtas.get(0),BorderLayout.CENTER);
 		
 		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -53,5 +71,15 @@ public class GistViewer extends JFrame {
 		
 			jtas.add(new JTextArea(file.getContent()));
 		}
+	}
+	private void fillJList(){
+		LinkedHashMap<String, GistFile> files = gist.getFiles();
+		Iterator<String> iterator = files.keySet().iterator();
+		
+		while(iterator.hasNext()){
+			String key = (String) iterator.next();
+			GistFile file = (GistFile)files.get(key);	
+			fileListModel.addElement(file.getFilename());
+		}	
 	}
 }
